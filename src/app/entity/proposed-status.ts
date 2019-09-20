247f81c4f7f81319a2fd4b43ed1df880d6239a04
Antti Tohmo <antti.tohmo@gofore.com>
2019-09-20 09:42:01 +0300

    YTI-941: Adding INCOMPLETE and SUGGESTED to proposable statuses for resources and own comments.

--- src/app/entity/proposed-status.ts
+++ src/app/entity/proposed-status.ts
@@ -11,4 +11,4 @@ export const allProposedStatuses =
   ['NOSTATUS', 'INCOMPLETE', 'DRAFT', 'SUGGESTED', 'VALID', 'SUPERSEDED', 'RETIRED', 'INVALID'] as ProposedStatus[];
 
 export const selectableProposedStatuses =
-  ['DRAFT', 'VALID', 'SUPERSEDED', 'RETIRED', 'INVALID'] as ProposedStatus[];
+  ['INCOMPLETE', 'DRAFT', 'SUGGESTED', 'VALID', 'SUPERSEDED', 'RETIRED', 'INVALID'] as ProposedStatus[];
