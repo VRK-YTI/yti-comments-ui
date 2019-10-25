dcb5ead5b98952d03bb3ce6e6afed192bc20a4e9
Antti Tohmo <antti.tohmo@gofore.com>
2019-10-25 15:26:58 +0300

    Loading logic bugfixed for messaging user data in user-details.

--- src/app/components/userdetails/user-details-subscriptions.component.ts
+++ src/app/components/userdetails/user-details-subscriptions.component.ts
@@ -41,8 +41,11 @@ export class UserDetailsSubscriptionsComponent implements OnInit {
 
   getUserSubscriptionData() {
 
+    this.loading = true;
+
     this.dataService.getMessagingUserData().subscribe(messagingUserData => {
-      this.loading = true;
+      this.loading = false;
+
       const resources = new Map<string, MessagingResource[]>();
       const codelistMessagingResources: MessagingResource[] = [];
       const datamodelMessagingResources: MessagingResource[] = [];
@@ -74,10 +77,8 @@ export class UserDetailsSubscriptionsComponent implements OnInit {
       }
       if (resources.size > 0) {
         this.messagingResources = resources;
-        this.loading = false;
       } else {
         this.messagingResources = null;
-        this.loading = false;
       }
     });
   }
